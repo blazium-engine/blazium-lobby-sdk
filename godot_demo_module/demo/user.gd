2dc7513aa20589c6ca51e1a188b7e6e817e74533
Dragos Daian <daian.dragos@yahoo.com>
2024-11-29 17:59:32 +0100

    upate methods to have lobbyPeer instead of string (#10)
    don't connect to localhost
    
    update code

--- godot_demo_module/demo/user.gd
+++ godot_demo_module/demo/user.gd
@@ -20,7 +20,7 @@ func _ready() -> void:
 	lobby_client.peer_named.connect(peer_named)
 	lobby_client.append_log.connect(append_log)
 
-	lobby_client.server_url = "ws://localhost:8080/connect"
+	#lobby_client.server_url = "ws://localhost:8080/connect"
 	lobby_client.connect_to_lobby("demo_game")
 
 func append_log(command: String, logs: String):
@@ -45,20 +45,20 @@ func lobby_joined(lobby: LobbyInfo, peers: Array[LobbyPeer]):
 func lobby_left():
 	print("Callback: %s lobby_left" % [get_index()])
 
-func peer_joined(peer_id: String, peer_name: String):
-	print("Callback: %s peer_joined %s %s" % [get_index(), peer_id, peer_name])
+func peer_joined(peer: LobbyPeer):
+	print("Callback: %s peer_joined %s %s" % [get_index(), peer.id, peer.peer_name])
 
-func peer_left(lobby_peer: String, kicked: bool):
-	print("Callback: %s peer_left %s %s" % [get_index(), lobby_peer, kicked])
+func peer_left(peer: LobbyPeer, kicked: bool):
+	print("Callback: %s peer_left %s %s" % [get_index(), peer.id, kicked])
 	
-func peer_ready(lobby_peer: String):
-	print("Callback: %s peer_ready %s" % [get_index(), lobby_peer])
+func peer_ready(peer: LobbyPeer):
+	print("Callback: %s peer_ready %s" % [get_index(), peer.id])
 
-func peer_unready(lobby_peer: String):
-	print("Callback: %s peer_unready %s" % [get_index(), lobby_peer])
+func peer_unready(peer: LobbyPeer):
+	print("Callback: %s peer_unready %s" % [get_index(), peer.id])
 
-func peer_named(peer_id: String, peer_name: String):
-	print("Callback: %s peer_named %s %s" % [get_index(), peer_id, peer_name])
+func peer_named(peer: LobbyPeer):
+	print("Callback: %s peer_named %s %s" % [get_index(), peer.id, peer.peer_name])
 
 func lobby_sealed():
 	print("Callback: %s lobby_sealed" % [get_index()])
@@ -71,7 +71,7 @@ func _on_button_pressed() -> void:
 	var message = message_text.text
 	match item:
 		"create_lobby":
-			var result : ViewLobbyResult = await lobby_client.create_lobby(message, 4).finished
+			var result : ViewLobbyResult = await lobby_client.create_lobby(message, 3, "").finished
 			if result.has_error():
 				print("Create Error %s: " % get_index(), result.error)
 			else:
