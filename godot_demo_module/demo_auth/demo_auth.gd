319599962efaac3f9c22c356ccc091179dc0eda9
Dragos Daian <daian.dragos@yahoo.com>
2024-12-16 18:21:04 +0100

    add auth demo

--- godot_demo_module/demo_auth/demo_auth.gd
+++ godot_demo_module/demo_auth/demo_auth.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@onready var user_scene := preload("res://demo/user.tscn")
+@onready var user_scene := preload("res://demo_auth/user_auth.tscn")
 @export var container : VBoxContainer
 @export var result_test: RichTextLabel
 
@@ -9,6 +9,5 @@ func _on_add_user_pressed() -> void:
 	new_user.result_text = result_test
 	container.add_child(new_user)
 
-
 func _on_change_scene_pressed() -> void:
-	get_tree().change_scene_to_file("res://main_auth.tscn")
+	get_tree().change_scene_to_file("res://main.tscn")
